d150bab6bb4a58e3e3d3a61825cb6251e456408b
Santas7 <andreypanyshkin03@gmail.com>
2023-04-10 13:30:08 +0400

    создал базовый файл для лабораторной работы, подключил необходимые библиотеки

---
+++ main.py
@@ -0,0 +1,18 @@
+import hashlib
+import time
+import random
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
+
+
+OPTIONS = {
+    'hash': '754a917a9c82f5247412006a5abe1c0eb76e1007',
+    'payment_system': 'Mastercard',
+    'card_type': 'Кредитная',
+    'bank': 'ВТБ',
+    'last_digits': '0758',
+    'hash_function': 'sha1'
+}
+
+if __name__ == '__main__':
+    pass
